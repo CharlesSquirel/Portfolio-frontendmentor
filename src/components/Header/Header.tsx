68c0b53c1d3edfc1c8c6e68e1c726c53a5d71579
Karol Wiewiórka <karol.wiewiorka@gmail.com>
2023-04-08 14:25:24 +0200

    sadasd

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { StyledHeader } from "./StyledHeader";
+
+const Header = () => {
+  return (
+    <StyledHeader>
+      <h4></h4>
+      <div>
+        <h1></h1>
+      </div>
+    </StyledHeader>
+  );
+};
+
+export default Header;
